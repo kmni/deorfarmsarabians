3888651fa0887762e7e1082e65a86824989a95b5
Tomas Musiol <tomas.musiol@gmail.com>
2014-08-18 14:52:25 +0200

    Carousel na titulni strance

--- scripts/custom.js
+++ scripts/custom.js
@@ -1,2 +1,36 @@
 $(document).ready(function() {
+
+    $('.slider').jcarousel({
+        wrap: 'circular'
+    });
+    /*
+    .jcarouselAutoscroll({
+            interval: 3000,
+            target: '+=1',
+            autostart: true
+        })
+    */
+
+
+    $('.slider__arrow-left')
+        .on('jcarouselcontrol:active', function() {
+            $(this).removeClass('inactive');
+        })
+        .on('jcarouselcontrol:inactive', function() {
+            $(this).addClass('inactive');
+        })
+        .jcarouselControl({
+            target: '-=1'
+        });
+
+    $('.slider__arrow-right')
+        .on('jcarouselcontrol:active', function() {
+            $(this).removeClass('inactive');
+        })
+        .on('jcarouselcontrol:inactive', function() {
+            $(this).addClass('inactive');
+        })
+        .jcarouselControl({
+            target: '+=1'
+        });
 });
